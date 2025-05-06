3d6d97853c8a6e9de50a63e201464dfef4ed1eea
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-05-06 15:09:01 +0300

    use rfq -> supplier  quotation

--- eval_app/data_management/doctype/file_3___quotation_request_supplier/file_3_data_importer.py
+++ eval_app/data_management/doctype/file_3___quotation_request_supplier/file_3_data_importer.py
@@ -2,6 +2,7 @@ from erpnext.stock.doctype.material_request.material_request import make_request
 from eval_app.data_management.doctype.import_csv.data_importer import DataImporter
 import frappe
 from eval_app.data_management.doctype.import_csv.csv_importer_service import make_row_import
+from erpnext.buying.doctype.request_for_quotation.request_for_quotation import make_supplier_quotation_from_rfq
 
 class File3DataImporter(DataImporter):
 
@@ -27,8 +28,12 @@ class File3DataImporter(DataImporter):
         if errors_count == 0:
             for ref in distinct_refs:
                 try:
-                    mr = frappe.get_doc("Request for Quotation", {"ref": ref, "docstatus":0})
-                    mr.submit()
+                    rfq = frappe.get_doc("Request for Quotation", {"ref": ref, "docstatus":0})
+                    rfq.submit()
+                    # Generer une supplier quotation
+                    sq = make_supplier_quotation_from_rfq(rfq.name)
+                    sq.insert()
+
                 except Exception as e:
                     raise Exception(f"Erreur lors du submit pour la référence {ref} : {str(e)}")
 
