64e33a720f5058b14518c8dad8e1a065c7e9bb15
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-05-20 08:09:07 +0300

    eval app prepa eval setup

--- eval_app/data_management/page/import_csv_form_page/import_csv_form_page.js
+++
@@ -1,8 +0,0 @@
-frappe.pages['import-csv-form-page'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'Page D\'importation de donnee',
-		single_column: true
-	});
-}
-
