54f62282258787a921c26b6c79faab3f001c1be4
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-06-04 09:08:53 +0300

    set import process to the model

--- eval_app/data_management/doctype/eval_import_v3/eval_import_v3.py
+++ eval_app/data_management/doctype/eval_import_v3/eval_import_v3.py
@@ -6,10 +6,13 @@ from frappe.model.document import Document
 
 
 class EvalImportV3(Document):
+	def start_files_import(self):
+		pass
 	def setup_files(self, emp_file, structure_file, salary_file):
 		self.emp_file = emp_file
 		self.structure_file = structure_file
 		self.salary_file = salary_file
+	
 	def check_files(self):
 		"""
 		Check if the files are provided and valid.
@@ -22,3 +25,8 @@ class EvalImportV3(Document):
 			return False, "Salary file is required."
 		
 		return True, "All files are valid."
+
+@frappe.whitelist()
+def start_import():
+	import_doc : EvalImportV3 = EvalImportV3()
+	return import_doc.start_files_import()
